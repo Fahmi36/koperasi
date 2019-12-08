a3f2fae4797e15c5f5b2b37f04735e54e114b09b
Fahmi36 <fauzifahmi55@gmail.com>
2019-12-08 12:50:20 +0700

    api

--- application/controllers/api/Logincontroller.php
+++ application/controllers/api/Logincontroller.php
@@ -1,16 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+header('Access-Control-Allow-Origin:*');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 
 class Logincontroller extends CI_Controller {
+function __construct() {
+        parent::__construct();
+        header('Access-Control-Allow-Origin:*');
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+        header('Access-Control-Max-Age: 1000');
+        header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 
+    }
 	public function apilogin()
 	{
-		$user = $this->db->get('hp');
+		$user = $this->input->get('id_kop');
 		$query = $this->db->get_where('anggota',array('no_anggota'=>$user));
 		if ($query->num_rows() > 0) {
 			$json = array('success'=>true,'id'=>$query->row()->no_anggota,'nama'=>$query->row()->nama);
 		}else{
-			$json = array('success'=>false)
+			$json = array('success'=>false);
 		}
 		echo json_encode($json);
 	}
