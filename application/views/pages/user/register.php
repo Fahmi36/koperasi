356dbc71c42a136c565b34973a5e46be803ef169
rizki130198 <rizkiajja130198@gmail.com>
2020-01-08 11:53:36 +0700

    update

--- application/views/pages/user/register.php
+++ application/views/pages/user/register.php
@@ -60,7 +60,20 @@
 
                     <div class="form-group">
                         <label for="pekerjaan" class="form-label required">Pekerjaan</label>
-                        <div class="chiller_cb">
+                        <div class="form-date-group">
+                            <div class="form-date-item" style="width: 100%;">
+                                <select id="pekerjaan" name="pekerjaan" style="width: 100%;">
+                                    <option value="pkk">PKK (Pemberdayaan & Kesejahteraan Keluarga)</option>
+                                    <option value="peng_rptra">Pengelola RPTRA</option>
+                                    <option value="pljp">PJLP (Penyedia Jasa Lainnya Orang Perorangan)</option>
+                                    <option value="asn">ASN (Aparatur Sipil Negara)</option>
+                                    <option value="pkt">PKT (Pengembangan Kawasan Terpadu)</option>
+                                    <option value="lainnya">Lainnya (Pengembangan Kawasan Terpadu)</option>
+                                </select>
+                                <span class="select-icon"><i class="ti-angle-down"></i></span>
+                            </div>
+                        </div>
+                        <!-- <div class="chiller_cb">
                             <input id="peng_rptra" name="pekerjaan" type="checkbox">
                             <label for="peng_rptra">Pengelola RPTRA, Kelurahan</label>
                             <span></span>
@@ -85,111 +98,111 @@
                         </div>
                         <div class="form-group">
                             <input type="text" name="lainnya" id="isi_peke3" disabled/>
-                        </div>
-                    </fieldset>
+                        </div> -->
+                    </div>
+                </fieldset>
 
-                    <h3>
-                        <span class="icon"><i class="ti-email"></i></span>
-                        <span class="title_text">Contact</span>
-                    </h3>
-                    <fieldset>
-                        <legend>
-                            <span class="step-heading">Contact Informaltion: </span>
-                            <span class="step-number">Step 2 / 3</span>
-                        </legend>
+                <h3>
+                    <span class="icon"><i class="ti-email"></i></span>
+                    <span class="title_text">Contact</span>
+                </h3>
+                <fieldset>
+                    <legend>
+                        <span class="step-heading">Contact Informaltion: </span>
+                        <span class="step-number">Step 2 / 3</span>
+                    </legend>
+                    <div class="form-group">
+                        <label for="alamat" class="form-label required">Alamat Lengkap</label>
+                        <input type="text" name="alamat" id="alamat"/>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="no_ktp" class="form-label required">Nomor Identitas (KTP/SIM)</label>
+                        <input type="number" name="no_ktp" id="no_ktp" />
+                    </div>
+
+                    <div class="form-row">
                         <div class="form-group">
-                            <label for="alamat" class="form-label required">Alamat Lengkap</label>
-                            <input type="text" name="alamat" id="alamat"/>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="no_ktp" class="form-label required">Nomor Identitas (KTP/SIM)</label>
-                            <input type="number" name="no_ktp" id="no_ktp" />
-                        </div>
-
-                        <div class="form-row">
-                            <div class="form-group">
-                                <label for="no_rumah" class="form-label">No Telpon (Rumah)</label>
-                                <input type="number" name="no_rumah" id="no_rumah" />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="no_hp" class="form-label required">No Telpon (Handphone)</label>
-                                <input type="number" name="no_hp" id="no_hp" />
-                            </div>
-                        </div>
-                    </fieldset>
-
-                    <h3>
-                        <span class="icon"><i class="ti-star"></i></span>
-                        <span class="title_text">Offical</span>
-                    </h3>
-                    <fieldset>
-                        <legend>
-                            <span class="step-heading">Offical Informaltion: </span>
-                            <span class="step-number">Step 3 / 3</span>
-                        </legend>
-                        <p>Dengan ini mengajukan permohonan unutuk menjadi anggota Koperasi PKK MELATI JAYA. Saya bersedia mentaati Anggaran Dasar, Anggaran Rumah Tangga, Kebijakan Pengurus serta peraturan lainnya yang belaku pada koperasi PKK MELATI JAYA dengan penuh tanggung jawab.</p>
-                        <h5>Bersama Formulir permohonan ini, saya sertakan :</h5>
-
-                        <div class="form-group">
-                            <label for="file_fotocopy" class="form-label required">Fotocopy KTP/SIM</label>
-                            <input type="file" name="file_fotocopy" id="file_fotocopy" />
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group">
-                                <label for="foto_1" class="form-label required">Foto Berwarna 3x4</label>
-                                <input type="file" name="foto_1" id="foto_1" />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="foto_2" class="form-label required">Foto Berwarna 2x3</label>
-                                <input type="file" name="foto_2" id="foto_2" />
-                            </div>
+                            <label for="no_rumah" class="form-label">No Telpon (Rumah)</label>
+                            <input type="number" name="no_rumah" id="no_rumah" />
                         </div>
 
                         <div class="form-group">
-                            <label for="sim_sukarela" class="form-label required">Penyetoran pertama anggota baru yang terdiri dari :</label>
-                            <label for="department" class="form-label">- Simpanan Pokok : Rp 1.000.000 (dibayar 1x selama menjadi anggota)</label>
-                            <label for="department" class="form-label">- Simpanan Wajib : Rp 25.000 (dibayar perbulan)</label>
-                            <label for="department" class="form-label">- Simpanan Sukarela : Rp 25.000 (dibayar perbulan minimal Rp 10.000)</label>
-                            <input type="number" name="sim_sukarela" id="sim_sukarela" placeholder="Simpanan Sukarela" />
+                            <label for="no_hp" class="form-label required">No Telpon (Handphone)</label>
+                            <input type="number" name="no_hp" id="no_hp" />
                         </div>
+                    </div>
+                </fieldset>
+
+                <h3>
+                    <span class="icon"><i class="ti-star"></i></span>
+                    <span class="title_text">Offical</span>
+                </h3>
+                <fieldset>
+                    <legend>
+                        <span class="step-heading">Offical Informaltion: </span>
+                        <span class="step-number">Step 3 / 3</span>
+                    </legend>
+                    <p>Dengan ini mengajukan permohonan unutuk menjadi anggota Koperasi PKK MELATI JAYA. Saya bersedia mentaati Anggaran Dasar, Anggaran Rumah Tangga, Kebijakan Pengurus serta peraturan lainnya yang belaku pada koperasi PKK MELATI JAYA dengan penuh tanggung jawab.</p>
+                    <h5>Bersama Formulir permohonan ini, saya sertakan :</h5>
+
+                    <div class="form-group">
+                        <label for="file_fotocopy" class="form-label required">Fotocopy KTP/SIM</label>
+                        <input type="file" name="file_fotocopy" id="file_fotocopy" />
+                    </div>
+                    <div class="form-row">
                         <div class="form-group">
-                            <label for="pembayaran" class="form-label required">Yang sudah dibayarkan secara :</label>
-                            <div class="funkyradio">
-                                <div class="funkyradio-danger">
-                                    <input type="radio" name="pembayaran" id="lunas"/>
-                                    <label for="lunas">Lunas</label>
-                                </div>
-                                <div class="funkyradio-danger">
-                                    <input type="radio" name="pembayaran" id="cicil" />
-                                    <label for="cicil">Cicil</label>
-                                </div>
+                            <label for="foto_1" class="form-label required">Foto Berwarna 3x4</label>
+                            <input type="file" name="foto_1" id="foto_1" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="foto_2" class="form-label required">Foto Berwarna 2x3</label>
+                            <input type="file" name="foto_2" id="foto_2" />
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="sim_sukarela" class="form-label required">Penyetoran pertama anggota baru yang terdiri dari :</label>
+                        <label for="department" class="form-label">- Simpanan Pokok : Rp 1.000.000 (dibayar 1x selama menjadi anggota)</label>
+                        <label for="department" class="form-label">- Simpanan Wajib : Rp 25.000 (dibayar perbulan)</label>
+                        <label for="department" class="form-label">- Simpanan Sukarela : Rp 25.000 (dibayar perbulan minimal Rp 10.000)</label>
+                        <input type="number" name="sim_sukarela" id="sim_sukarela" placeholder="Simpanan Sukarela" />
+                    </div>
+                    <div class="form-group">
+                        <label for="pembayaran" class="form-label required">Yang sudah dibayarkan secara :</label>
+                        <div class="funkyradio">
+                            <div class="funkyradio-danger">
+                                <input type="radio" name="pembayaran" id="lunas"/>
+                                <label for="lunas">Lunas</label>
+                            </div>
+                            <div class="funkyradio-danger">
+                                <input type="radio" name="pembayaran" id="cicil" />
+                                <label for="cicil">Cicil</label>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="metode_pem" class="form-label required">Metode pembayaran melalui :</label>
-                            <div class="funkyradio">
-                                <div class="funkyradio-danger">
-                                    <input type="radio" name="metode_pem" id="petugas"/>
-                                    <label for="petugas">Petugas unit simpan pinjam</label>
-                                </div>
-                                <div class="funkyradio-danger">
-                                    <input type="radio" name="metode_pem" id="transfer" />
-                                    <label for="transfer">Transfer ke <b>Bank DKI dengan No Rekening 108-13-17099-8</b></label>
-                                </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="metode_pem" class="form-label required">Metode pembayaran melalui :</label>
+                        <div class="funkyradio">
+                            <div class="funkyradio-danger">
+                                <input type="radio" name="metode_pem" id="petugas"/>
+                                <label for="petugas">Petugas unit simpan pinjam</label>
                             </div>
-                            <input type="number" name="sebesar" id="sebesar" placeholder="Sebesar" />
+                            <div class="funkyradio-danger">
+                                <input type="radio" name="metode_pem" id="transfer" />
+                                <label for="transfer">Transfer ke <b>Bank DKI dengan No Rekening 108-13-17099-8</b></label>
+                            </div>
                         </div>
-                    </fieldset>
-                </form>
-            </div>
-
-        </div>
-        <div class="footer">
-
+                        <input type="number" name="sebesar" id="sebesar" placeholder="Sebesar" />
+                    </div>
+                </fieldset>
+            </form>
         </div>
 
-        <!-- JS -->
-        
+    </div>
+    <div class="footer">
+
+    </div>
+
+    <!-- JS -->
